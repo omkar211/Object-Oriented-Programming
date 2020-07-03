90763be29d7a52a6f7cfbb99ca7e53baffb552aa
omkar sharma <40177529+omkar211@users.noreply.github.com>
2020-07-04 01:48:54 +0530

    Add files via upload

---
+++ Builder_pattern.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class leopard{
+    public:
+    string color;
+    string name;
+    string gender;
+   
+    leopard(X obj)
+    {
+       this->color=obj.color;
+       this->name=obj.name;
+       this->gender=obj.gender;
+    }
+
+
+    class static builder{
+    public:
+    string color;
+    string name;
+    string gender;
+    
+    void set_color(string color)
+    {
+        this->color=color;
+    }
+    void set_name(string name)
+    {
+        this->name=name;
+    }
+    void set_gender(string gender)
+    {
+        this->gender=gender;
+    }
+     leopard build()
+     {
+         leopard l=new leopard(this);
+         return l;
+     }
+};
+
+
+};
+int main()
+{
+  builder obj=new leopard.builder().set_name("lea").set_color("green").build();
+  
+  
+}
